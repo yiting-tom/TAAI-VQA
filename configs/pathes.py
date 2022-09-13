fd9a9dba737a587d004b011dcdafb56a2d5bfe0d
Yi-Ting Li <liyitingtom@gmail.com>
2022-09-13 23:59:50 +0000

    feat: add some pathes

--- configs/pathes.py
+++ configs/pathes.py
@@ -1,7 +1,25 @@
 from pathlib import Path
 from typing import Final
 
-ROOT: Final[Path] = Path(__file__).parent.parent
-LOGS: Final[Path] = ROOT / 'logs'
-DATA: Final[Path] = ROOT / 'data'
-COCO: Final[Path] = DATA / 'COCO'
+# ========================= VQA =========================
+d_ROOT:                 Final[Path] = Path(__file__).parent.parent
+d_LOGS:                 Final[Path] = d_ROOT / 'logs'
+d_DATA:                 Final[Path] = d_ROOT / 'data'
+
+# ===================== VQA & VQA-E ======================
+d_VQA:                  Final[Path] = d_DATA / 'vqa'
+d_VQAE:                 Final[Path] = d_DATA / 'vqa-e'
+
+# ========================= COCO  ========================
+d_COCO:                 Final[Path] = d_DATA / 'COCO'
+d_COCO_FEATURE:         Final[Path] = d_COCO / 'feature_36'
+d_COCO_BBOX:            Final[Path] = d_COCO / 'bbox_36'
+d_COCO_GRAPH:           Final[Path] = d_COCO / 'graph_36'
+
+# ========================= GloVe ========================
+d_GLOVE:                Final[Path] = d_DATA / 'glove.6B'
+
+# ======================= Generated ======================
+d_ANNOTATIONS:          Final[Path] = d_DATA / 'annotations'
+f_CANDIDATE_ANSWERS:    Final[Path] = d_DATA / 'candidate_answers.txt'
+f_GOLVE_VOCABULARIES:   Final[Path] = d_DATA / 'glove_vocabularies.txt'
