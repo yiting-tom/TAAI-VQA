111ab2a294ad82cd568aa4a352ed09c92a866e71
Yi-Ting Li <liyitingtom@gmail.com>
2022-09-13 23:59:25 +0000

    feat: add logging and replace the pathes by configs.pathes

--- data_preparing/generate_glove_vocabularies.py
+++ data_preparing/generate_glove_vocabularies.py
@@ -1,13 +1,12 @@
 from pathlib import Path
 from typing import List
 from tqdm import tqdm
-import logging
-
-logging.basicConfig(format="[%(levelname)s] %(message)s", level=logging.DEBUG)
+from configs import pathes
+from configs.logger import l
 
 def load_glove_vocabulary(glove_file: Path) -> List:
     """Load GloVe vocabulary"""
-    logging.info('Loading GloVe vocabulary ...')
+    l.info('Loading GloVe vocabulary ...')
     vocab = []
     with open(glove_file) as f:
         load = f.read().split('\n')
@@ -19,23 +18,22 @@ def load_glove_vocabulary(glove_file: Path) -> List:
 
 def save_glove_vocabulary(vocab: List, save_path: Path) -> None:
     """Save GloVe vocabulary to file"""
-    logging.info('Saving GloVe vocabulary')
+    l.info('Saving GloVe vocabulary')
     with open(save_path, 'w') as f:
         f.write('\n'.join(vocab))
 
 def main():
-    logging.info("Generating GloVe vocabulary ...")
+    l.info("Generating GloVe vocabulary ...")
     vocab = load_glove_vocabulary(
-        glove_file=Path('/home/P76104419/VQA/Thesis-VQA/data/glove.6B/glove.6B.300d.txt')
+        glove_file=pathes.f_GLOVE/'glove.6B.300d.txt',
     )
 
     vocab += ['<start>', '<end>', '<pad>', '<oov>']
 
     save_glove_vocabulary(
         vocab=vocab,
-        save_path=Path('/home/P76104419/VQA/Thesis-VQA/data/glove_vocabularies.txt')
+        save_path=pathes.f_GOLVE_VOCABULARIES,
     )
-    logging.info("Done")
 
 if __name__ == '__main__':
     main()
